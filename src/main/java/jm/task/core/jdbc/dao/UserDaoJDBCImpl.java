e1ea84d1c2b855b65084f40383ce449f6775135b
Rastre <rastor13666gmail.com>
2023-03-08 07:42:39 +0300

    JDBC Project

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,95 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+    private static Connection connection = Util.getMyConnection();
+    private static String option;
+    private static String errorMessage;
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        option = "create table UsersTable(" + "id bigint auto_increment primary key,"
+                + "name varchar(30)," + "lastName varchar(30)," + "age tinyint)";
+        errorMessage = "A table with the same name already exists";
+        doOperation();
     }
 
     public void dropUsersTable() {
-
+        option = "drop table UsersTable";
+        errorMessage = "The table you are trying to delete doesn't exist";
+        doOperation();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        option = "insert into UsersTable(name, lastName, age) values('" +
+                name + "', '" + lastName + "', " + age + ")";
+        errorMessage = "User hasn't been saved to the database";
+        doOperation();
     }
 
     public void removeUserById(long id) {
-
-    }
-
-    public List<User> getAllUsers() {
-        return null;
+        option = "delete from UsersTable where id = " + id;
+        errorMessage = "User hasn't been deleted from the database";
+        doOperation();
     }
 
     public void cleanUsersTable() {
+        option = "truncate UsersTable";
+        errorMessage = "Database hasn't been cleaned";
+        doOperation();
+    }
 
+    public List<User> getAllUsers() {
+        List<User> list = new ArrayList<>();
+        option = "select * from UsersTable";
+        try (Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
+            ResultSet resultSet = statement.executeQuery(option);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getByte(4));
+                System.out.println(user);
+                list.add(user);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            catchCase();
+        }
+        return list;
+    }
+
+    public void doOperation() {
+        try (Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
+            statement.executeUpdate(option);
+            connection.commit();
+        } catch (SQLException e) {
+            catchCase();
+        }
+    }
+
+    public void catchCase() {
+        System.out.println(errorMessage);
+        try {
+            connection.rollback();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/testbase";
+    private static final String LOGIN = "root";
+    private static final String PASSWORD = "i@dijwfdqa2";
+    private static Connection connection;
+
+    public static Connection getMyConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeMyConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
